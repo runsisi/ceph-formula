9d497e2ff5fee2cd5b5fa0e3a426a04169cf28e4
runsisi <runsisi@hust.edu.cn>
2015-05-05 10:25:50 +0800

    fix package can not be run on VT100 error
    Signed-off-by: runsisi <runsisi@hust.edu.cn>

--- pkg/binhdr.sh
+++ pkg/binhdr.sh
@@ -4,24 +4,6 @@
 
 set -e
 
-# http://stackoverflow.com/questions/4332478/read-the-current-text-
-# color-in-a-xterm/4332530#4332530
-BLACK=$(tput setaf 0)
-RED=$(tput setaf 1)
-GREEN=$(tput setaf 2)
-YELLOW=$(tput setaf 3)
-LIME_YELLOW=$(tput setaf 190)
-POWDER_BLUE=$(tput setaf 153)
-BLUE=$(tput setaf 4)
-MAGENTA=$(tput setaf 5)
-CYAN=$(tput setaf 6)
-WHITE=$(tput setaf 7)
-BRIGHT=$(tput bold)
-NORMAL=$(tput sgr0)
-BLINK=$(tput blink)
-REVERSE=$(tput smso)
-UNDERLINE=$(tput smul)
-
 BOOTSTRAP=clove/bootstrap.py
 
 trap 'cleanup' EXIT
@@ -32,20 +14,20 @@ cleanup() {
 }
 
 logerror() {
-    printf "${RED}[ERROR] ${NORMAL}%s\nExiting now!\n" "$1" 1>&2
+    printf "[ERROR] %s\nExiting now!\n" "$1" 1>&2
     exit 1
 }
 
 logwarning() {
-    printf "${MAGENTA}[WARN ] ${NORMAL}%s\n" "$1"
+    printf "[WARN ] %s\n" "$1"
 }
 
 loginfo() {
-    printf "${BLUE}[INFO ] ${NORMAL}%s\n" "$1"
+    printf "[INFO ] %s\n" "$1"
 }
 
 logdebug() {
-    printf "${BLACK}[DEBUG] ${NORMAL}%s\n" "$1"
+    printf "[DEBUG] %s\n" "$1"
 }
 
 lower() {
